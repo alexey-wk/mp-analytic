3d763b391cfa5e212c55ff63c95a716aa6d69260
alexey_wk <comin.alexej@yandex.ru>
2025-06-12 13:05:09 +0300

    добавлена ручка хелсчека

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
-from app.repository.db import init_db
 from app.controller.rnp import rnp_router
+from app.controller.health import health_router
 from app.controller.product_report import product_report_router
 from app.controller.seller_account import seller_account_router
 from app.lifespan import lifespan
@@ -11,3 +11,4 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(rnp_router)
 app.include_router(product_report_router)
 app.include_router(seller_account_router)
+app.include_router(health_router)
