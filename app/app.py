ada7d94131d94abd36d6e60b75499922b6d2349c
alexey_wk <comin.alexej@yandex.ru>
2025-06-17 11:26:56 +0300

    разрешение по cors

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.controller.rnp import rnp_router
 from app.controller.health import health_router
 from app.controller.product_report import product_report_router
@@ -7,6 +8,14 @@ from app.controller.seller_account import seller_account_router
 from app.lifespan import lifespan
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(rnp_router)
 app.include_router(product_report_router)
