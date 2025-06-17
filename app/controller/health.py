fced0d8cec90df206492352cbec9675c9af9bbc9
alexey_wk <comin.alexej@yandex.ru>
2025-06-17 11:13:54 +0300

    изменен порт запуска 2

--- app/controller/health.py
+++ app/controller/health.py
@@ -1,7 +1,10 @@
+import logging
 from fastapi import APIRouter
 
+logger = logging.getLogger(__name__)
 health_router = APIRouter()
 
 @health_router.get("/health")
 async def health():
+    logger.info("Health check")
     return {}
